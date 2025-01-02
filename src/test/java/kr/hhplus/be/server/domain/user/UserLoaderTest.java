543040339c1663c0d927ecdb996b284e7cef5f1e
justin.jeon <kergosdyr@kakao.com>
2025-01-02 14:02:44 +0900

    test(domain): add domain balance unit test

---
+++ src/test/java/kr/hhplus/be/server/domain/user/UserLoaderTest.java
@@ -0,0 +1,58 @@
+package kr.hhplus.be.server.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kr.hhplus.be.server.error.ApiException;
+
+@ExtendWith(MockitoExtension.class)
+class UserLoaderTest {
+
+	@Mock
+	UserRepository userRepository;
+
+	@InjectMocks
+	UserLoader userLoader;
+
+	@Test
+	@DisplayName("유저가 존재하지 않으면, ApiException을 발생시키고 User를 찾을 수 없다.")
+	void shouldUserNotExistThrowApiException() {
+
+		// given
+		Mockito.when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+		// when
+		// then
+		assertThatThrownBy(() -> {
+			userLoader.load(1L);
+		}).isInstanceOf(ApiException.class).hasMessageContaining("요청하신 유저를 찾을 수 없습니다");
+
+	}
+
+	@Test
+	@DisplayName("유저가 존재하면, User를 반환한다.")
+	void shouldUserExistReturnUser() {
+
+		// given
+		User user = User.builder().userId(1L).build();
+		Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+		// when
+		User loadedUser = userLoader.load(1L);
+
+		// then
+		Assertions.assertThat(user).isEqualTo(loadedUser);
+
+	}
+
+}
