7a9fcdf5aa95c8cadca24d9bffca18803ab8a1e5
kergosdyr <kergosdyr@kakao.com>
2025-01-05 13:09:56 +0900

    feature(product): add ProductRepository

---
+++ src/main/java/kr/hhplus/be/server/domain/product/ProductRepository.java
@@ -0,0 +1,16 @@
+package kr.hhplus.be.server.domain.product;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface ProductRepository {
+	List<Product> findAll(int page, int size);
+
+	List<Product> findAllByKeyword(String keyword, int page, int size);
+
+	long countAll();
+
+	long countAllByKeyword(String keyword);
+}
