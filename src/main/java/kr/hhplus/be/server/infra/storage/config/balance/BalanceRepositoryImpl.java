f25b244c1a91ecd331f8463b8daa0c94ec760503
kergosdyr <kergosdyr@kakao.com>
2025-01-05 11:52:43 +0900

    feature(balance): add pessimistic lock to BalanceJpaRepository

--- src/main/java/kr/hhplus/be/server/infra/storage/config/balance/BalanceRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infra/storage/config/balance/BalanceRepositoryImpl.java
@@ -6,10 +6,17 @@ import org.springframework.stereotype.Component;
 
 import kr.hhplus.be.server.domain.balanace.Balance;
 import kr.hhplus.be.server.domain.balanace.BalanceRepository;
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class BalanceRepositoryImpl implements BalanceRepository {
+
+	private final BalanceJpaRepository balanceJpaRepository;
+
 	@Override
 	public Optional<Balance> findByUserId(long userId) {
-		return Optional.empty();
+		return balanceJpaRepository.findByUserIdWithLock(userId);
 	}
+
 }
