e910c58dc9257ce8f97ed2dec66e2c079227a497
kergosdyr <kergosdyr@kakao.com>
2025-01-06 00:01:52 +0900

    feature(querydsl): 쿼리dsl customRepo 수정

--- src/main/java/kr/hhplus/be/server/infra/storage/product/ProductRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infra/storage/product/ProductRepositoryImpl.java
@@ -1,16 +1,9 @@
 package kr.hhplus.be.server.infra.storage.product;
 
-import static kr.hhplus.be.server.domain.order.QOrder.order;
-import static kr.hhplus.be.server.domain.order.QOrderDetail.orderDetail;
-import static kr.hhplus.be.server.domain.product.QProduct.product;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import kr.hhplus.be.server.domain.product.Product;
 import kr.hhplus.be.server.domain.product.ProductRepository;
@@ -18,11 +11,9 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class ProductRepositoryImpl implements ProductRepository, ProductQueryDslRepository {
+public class ProductRepositoryImpl implements ProductRepository {
 
 	private final ProductJpaRepository productJpaRepository;
-
-	private final JPAQueryFactory queryFactory;
 
 
 
@@ -48,19 +39,7 @@ public class ProductRepositoryImpl implements ProductRepository, ProductQueryDsl
 
 	@Override
 	public List<Product> findAllTopSellers(int days) {
-
-		LocalDateTime startDate = LocalDateTime.now().minusDays(days);
-
-		return queryFactory
-			.select(product)
-			.from(order)
-			.join(order.orderDetails, orderDetail)
-			.join(orderDetail.product, product)
-			.where(order.createdAt.goe(startDate))
-			.groupBy(product.productId)
-			.orderBy(orderDetail.quantity.sum().desc())
-			.limit(5)
-			.fetch();
-
+		return productJpaRepository.findAllTopSellers(days);
 	}
+
 }
