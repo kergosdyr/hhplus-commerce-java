3a8367bc21001c20fe77d0aec07b572ad9aaaa0d
kergosdyr <kergosdyr@kakao.com>
2025-01-10 07:31:11 +0900

    test(order): edit order test without coupon, with coupon

--- src/test/java/kr/hhplus/be/server/api/OrderControllerTest.java
+++ src/test/java/kr/hhplus/be/server/api/OrderControllerTest.java
@@ -75,6 +75,84 @@ class OrderControllerTest extends WebIntegrationTest {
 
 		var mockOrderPayment = new OrderPayment(mockOrder, mockPayment);
 
+		Mockito.when(orderService.orderWithCoupon(Mockito.anyLong(), Mockito.anyLong(), Mockito.anyList()))
+			.thenReturn(mockOrderPayment);
+
+		// when & then
+		mockMvc.perform(
+				post("/api/v1/order")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+			)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.orderId").value(1L))
+			.andExpect(jsonPath("$.data.userId").value(123L))
+			.andExpect(jsonPath("$.data.status").value("PAID"))
+			.andExpect(jsonPath("$.data.totalAmount").value(2000L))
+			.andExpect(jsonPath("$.data.discountAmount").value(100L))
+			.andExpect(jsonPath("$.data.paidAmount").value(1800L))
+			.andExpect(jsonPath("$.data.paymentId").value(1L))
+			.andExpect(jsonPath("$.data.paymentStatus").value("PAID"))
+			.andExpect(jsonPath("$.data.orderItems.length()").value(2))
+			.andExpect(jsonPath("$.data.orderItems[0].productId").value(1L))
+			.andExpect(jsonPath("$.data.orderItems[0].quantity").value(2L))
+			.andExpect(jsonPath("$.data.orderItems[0].price").value(500L))
+			.andExpect(jsonPath("$.data.orderItems[1].productId").value(2L))
+			.andExpect(jsonPath("$.data.orderItems[1].quantity").value(3L))
+			.andExpect(jsonPath("$.data.orderItems[1].price").value(300L));
+	}
+
+	@Test
+	@DisplayName("[POST] /api/v1/order - 주문 생성/결제 성공 테스트(쿠폰 없음)")
+	void createOrderSuccessTestWithoutCoupon() throws Exception {
+		// given
+		var orderItems = List.of(
+			new OrderRequest.OrderItem(2L, 1L), // Product ID: 1, Quantity: 2
+			new OrderRequest.OrderItem(3L, 2L)  // Product ID: 2, Quantity: 3
+		);
+
+		var request = new OrderRequest(123L, null, orderItems); // userId, couponId, orderItems
+
+		var mockOrderDetails = List.of(
+			OrderDetail.builder()
+				.productId(1L)
+				.quantity(2L)
+				.product(Product.builder()
+					.productId(1L)
+					.price(500L)
+					.build())
+				.build(),
+			OrderDetail.builder()
+				.productId(2L)
+				.quantity(3L)
+				.product(Product.builder()
+					.productId(2L)
+					.price(300L)
+					.build())
+				.build()
+		);
+
+		var mockOrder = Order.builder()
+			.orderId(1L)
+			.userId(123L)
+			.total(2000L)
+			.status(OrderStatus.PAID)
+			.orderDetails(mockOrderDetails)
+			.build();
+
+		var mockPayment = Payment.builder()
+			.paymentId(1L)
+			.orderId(1L)
+			.userId(123L)
+			.isUsedCoupon(true)
+			.totalPrice(1800L)
+			.couponId(456L)
+			.couponUsedPrice(200L)
+			.status(PaymentStatus.PAID)
+			.build();
+
+		var mockOrderPayment = new OrderPayment(mockOrder, mockPayment);
+
 		Mockito.when(orderService.order(Mockito.anyLong(), Mockito.anyList()))
 			.thenReturn(mockOrderPayment);
 
