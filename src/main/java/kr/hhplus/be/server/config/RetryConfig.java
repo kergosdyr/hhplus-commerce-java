d0869c1fbdd8364b7418124b423d51ae935162c0
kergosdyr <kergosdyr@kakao.com>
2025-01-24 00:04:07 +0900

    delete(config): delete unnecessary config files

--- src/main/java/kr/hhplus/be/server/config/RetryConfig.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
-
-@Configuration
-@EnableRetry
-public class RetryConfig {
-}
