eb4a8961129ff9f83f8d48cefeee9b688e22d891
kergosdyr <kergosdyr@kakao.com>
2025-02-07 07:19:42 +0900

    feature(error): add error type for wait list

--- src/main/java/kr/hhplus/be/server/error/ErrorType.java
+++ src/main/java/kr/hhplus/be/server/error/ErrorType.java
@@ -7,21 +7,18 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorType {
-	DEFAULT_ERROR(
-		HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "예상치 못한 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.", LogLevel.ERROR),
-	USER_NOT_FOUND(
-		HttpStatus.NOT_FOUND, ErrorCode.E400, "요청하신 유저를 찾을 수 없습니다", LogLevel.ERROR),
-	BALANCE_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500,
-		"잔액 충전 처리 중 오류가 발생했습니다.", LogLevel.ERROR), COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E400,
-		"요청하신 쿠폰을 찾을 수 없습니다", LogLevel.ERROR),
-	COUPON_ALREADY_ISSUED(HttpStatus.CONFLICT, ErrorCode.E400, "쿠폰은 중복해서 발급할 수 없습니다",
-		LogLevel.ERROR), COUPON_NOT_ISSUABLE(
-		HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "쿠폰이 만료되었거나 모두 소진되었습니다.", LogLevel.ERROR),
-
+	DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "예상치 못한 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.",
+		LogLevel.ERROR),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E400, "요청하신 유저를 찾을 수 없습니다", LogLevel.ERROR),
+	BALANCE_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "잔액 충전 처리 중 오류가 발생했습니다.", LogLevel.ERROR),
+	COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E400, "요청하신 쿠폰을 찾을 수 없습니다", LogLevel.ERROR),
+	COUPON_ALREADY_ISSUED(HttpStatus.CONFLICT, ErrorCode.E400, "쿠폰은 중복해서 발급할 수 없습니다", LogLevel.ERROR),
+	COUPON_WAIT_ALREADY_ISSUED(HttpStatus.CONFLICT, ErrorCode.E400, "해당 쿠폰은 발급 요청 중입니다", LogLevel.ERROR),
+	COUPON_NOT_ISSUABLE(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "쿠폰이 만료되었거나 모두 소진되었습니다.", LogLevel.ERROR),
 	BALANCE_OVER_USE(HttpStatus.NOT_ACCEPTABLE, ErrorCode.E400, "잔액을 초과해서 사용할 수 없습니다", LogLevel.ERROR),
-	BAD_REQUEST_ERROR(HttpStatus.BAD_REQUEST, ErrorCode.E400, "잘못된 요청 정보를 전송하셨습니다.", LogLevel.ERROR), PRODUCT_NOT_FOUND(
-		HttpStatus.NOT_FOUND, ErrorCode.E400, "해당하는 상품이 존재하지 않습니다", LogLevel.ERROR), PRODUCT_NO_STOCK(
-		HttpStatus.BAD_REQUEST, ErrorCode.E400, "해당 상품은 품절되었습니다", LogLevel.ERROR),
+	BAD_REQUEST_ERROR(HttpStatus.BAD_REQUEST, ErrorCode.E400, "잘못된 요청 정보를 전송하셨습니다.", LogLevel.ERROR),
+	PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E400, "해당하는 상품이 존재하지 않습니다", LogLevel.ERROR),
+	PRODUCT_NO_STOCK(HttpStatus.BAD_REQUEST, ErrorCode.E400, "해당 상품은 품절되었습니다", LogLevel.ERROR),
 	ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E400, "해당하는 주문이 존재하지 않습니다", LogLevel.ERROR),
 	;
 
